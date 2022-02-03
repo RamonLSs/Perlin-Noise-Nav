f2ca75585f0e65daf66e9a6999e2405660b74108
Ramon Lopez Salmeron <ramoncillo1409@gmail.com>
2022-02-03 20:30:21 +0100

    Commit
    Creacion de un mapa aleatorio, un personaje y navmesh

--- Assets/Scripts/GeneradorPerlinNoise.cs
+++ Assets/Scripts/GeneradorPerlinNoise.cs
@@ -7,10 +7,13 @@ public class GeneradorPerlinNoise : MonoBehaviour
     public int x, z;
     public GameObject pieza, personaje;
     public NavMeshSurface superficie;
-    
+    private bool existePersonaje;
+
     void Start()
     {
-        Generar();
+        existePersonaje = false;
+        GenerarMapa();
+        GenerarPersonaje();
         Invoke("GenerarNavMesh", 10);
     }
 
@@ -19,12 +22,12 @@ public class GeneradorPerlinNoise : MonoBehaviour
         superficie.BuildNavMesh();
     }
 
-    private void Generar()
+    private void GenerarMapa()
     {
         float semilla = Random.Range(0.0f, 1000.0f);
         float escala = 10.0f;
         int proporcion = 50;
-        bool existePersonaje = false;
+        
 
         for (int i = 0; i < x; i++)
         {
@@ -38,14 +41,39 @@ public class GeneradorPerlinNoise : MonoBehaviour
                 {
                     Instantiate(pieza, new Vector3(i * 5, 0, j * 5), Quaternion.identity);
 
-                    int genP = Random.Range(0, 10);
+                   /* int genP = Random.Range(0, 10);
 
                     if(genP == 5 && existePersonaje == false)
                     {
                         Instantiate(personaje, new Vector3(i * 5, 1.5f, j * 5), Quaternion.identity);
                         existePersonaje = true;
-                    }
+                    }*/
                 }
+            }
+        }
+    }
+
+    private void GenerarPersonaje()
+    {
+        int iteraciones = 0;
+        while (existePersonaje == false)
+        {
+            if(iteraciones == 1000)
+            {
+                break;
+            }
+            iteraciones++;
+            Debug.Log(iteraciones);
+
+            int CoorX = Random.Range(0, (x*5)+1);
+            int CoorZ = Random.Range(0, (z * 5) + 1);
+
+            Vector3 IniciarRayo = new Vector3(CoorX, 6, CoorZ);
+
+            if (Physics.Raycast(IniciarRayo, Vector3.down, 10))
+            {
+                Instantiate(personaje, new Vector3(CoorX, 3.5f, CoorZ), Quaternion.identity);
+                existePersonaje = true;
             }
         }
     }
